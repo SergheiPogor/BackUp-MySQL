725f9a1106ce692e1f36bcf5bb01cd64e345b982
Serghei Pogor <mediapronetsrl@gmail.com>
2019-05-16 23:47:32 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,5 +1,13 @@
 <?php
-$connection = mysqli_connect("localhost", "user", "password", "database");
+
+// database credits
+ $db_host   = "localhost";
+ $db_user   = "root";
+ $db_pass   = "1234";
+ $db        = "example";
+
+
+$connection = mysqli_connect($dbhost, $db_user, $db_pass, $db);
 
 if (!$connection) {
     echo "Error: Unable to connect to MySQL => ".mysqli_connect_errno();
